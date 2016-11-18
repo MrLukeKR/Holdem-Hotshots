dcae22df699392382e7477f8ecada5ca5713c0e6
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-18 18:51:16 +0000

    Implemented a "Transferrable" interface and extracted the abstract class "GameEntity" to be extended by Player and Table
    Former-commit-id: 3783b304c2ad5ebcf6b8b87f20d52e15a7484168

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerObjects/Table.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerObjects/Table.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldemPoker.Game.PokerObjects;
 
 namespace MixedRealityPoker.Game.PokerObjects
 {
-    class Table
+    class Table : GameEntity
     {
     }
 }
