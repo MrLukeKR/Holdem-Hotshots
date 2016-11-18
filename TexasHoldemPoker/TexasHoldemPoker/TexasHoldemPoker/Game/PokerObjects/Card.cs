c5cba372d127371917a5907ea84a51830767792c
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-18 01:22:25 +0000

    Essential (unimplemented) classes added
    Former-commit-id: 7f99bccebad2664c4c27c954bc54e2304c4d00c5

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerObjects/Card.cs
@@ -0,0 +1,19 @@
+﻿using MixedRealityPoker.Game.RenderEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixedRealityPoker.Game.PokerObjects
+{
+    class Card
+    {
+        private Model cardModel;
+
+        public Card(int suit, int value) //Provide handling of models/textures for the card
+        {
+
+        }
+    }
+}
