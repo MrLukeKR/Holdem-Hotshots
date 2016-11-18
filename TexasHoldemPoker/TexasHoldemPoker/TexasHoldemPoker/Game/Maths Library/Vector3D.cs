9c68d2c731e4075c1c2fe8cb1847c43128e8fb92
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-18 01:22:25 +0000

    Essential (unimplemented) classes added
    Former-commit-id: 7f99bccebad2664c4c27c954bc54e2304c4d00c5

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/Maths Library/Vector3D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixedRealityPoker.Game.Maths_Library
+{
+    class Vector3D
+    {
+    }
+}
---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerObjects/Lobby.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixedRealityPoker.Game.PokerObjects
+{
+    class Lobby
+    {
+        private int numberOfPlayers;
+        private Player[] players;
+
+        public Lobby(int numberOfPlayers)
+        {
+            this.numberOfPlayers = numberOfPlayers;
+            players = new Player[numberOfPlayers];
+        }
+    }
+}
