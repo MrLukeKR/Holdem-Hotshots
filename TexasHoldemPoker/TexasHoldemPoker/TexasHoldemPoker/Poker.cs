09241b3b502a5a3df41c5df3b8bb4540c697cdf3
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-19 03:40:42 +0000

    UrhoSharp is now stable and ready to program with

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Poker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Urho;
+
+namespace TexasHoldemPoker
+{
+    class Poker : Urho.Application
+    {
+        public Poker(ApplicationOptions options = null) : base(options) { }
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void OnUpdate(float timeStep)
+        {
+            base.OnUpdate(timeStep);
+        }
+    }
+}
