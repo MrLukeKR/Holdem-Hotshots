9f0d8060fda703b6886554ad70816068d0e76bbf
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-19 23:02:57 +0000

    Setup placeholders for the app icon

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/Properties/AssemblyInfo.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/Properties/AssemblyInfo.cs
@@ -6,15 +6,16 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TexasHoldemPoker.Droid")]
+[assembly: AssemblyTitle("Texas Hold 'em Poker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TexasHoldemPoker.Droid")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("The University of Nottingham")]
+[assembly: AssemblyProduct("TexasHoldemPoker")]
+[assembly: AssemblyCopyright("Copyright © Advantage Software Group 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+[assembly: Application(Icon = "@drawable/icon")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,8 +27,8 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
