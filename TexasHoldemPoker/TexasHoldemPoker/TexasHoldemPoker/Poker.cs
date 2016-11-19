d6f764918716b4d071a40e86b6589d2322b57ac6
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-19 22:39:06 +0000

    Menu BGM implemented

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Poker.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Poker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Urho;
+using Urho.Audio;
 using Urho.Gui;
 using Urho.Urho2D;
 
@@ -11,11 +12,28 @@ namespace TexasHoldemPoker
     {
         public Poker() : base(new ApplicationOptions(assetsFolder: "Data")) { }
 
+        
         protected override void Start()
         {
             base.Start();
             var cache = ResourceCache;
-          
+            Scene menuScene = new Scene();
+
+            menuScene.CreateComponent<Octree>();
+
+            Sound BGM = cache.GetSound("Sounds/MenuBGM.wav");
+            BGM.Looped = true;
+
+            SoundSource musicSource = new SoundSource();
+
+            
+            menuScene.AddComponent(musicSource);
+
+           // musicSource.Gain = 1;
+           
+            musicSource.Play(BGM);
+            
+
             Graphics.SetWindowIcon(cache.GetImage("Textures/GameIcon.png"));
             Graphics.WindowTitle = "Texas Hold 'em Poker";
 
@@ -27,18 +45,18 @@ namespace TexasHoldemPoker
             };
 
             copyrightNotice.SetColor(new Color(1.0f, 1.0f, 1.0f, 0.8f));
-            copyrightNotice.SetFont(cache.GetFont("Fonts/arial.ttf"), 15);
+            copyrightNotice.SetFont(cache.GetFont("Fonts/arial.ttf"), 10);
 
             var gameTitle = new BorderImage();
             gameTitle.Texture = cache.GetTexture2D("Textures/gameTitle.png");
             gameTitle.BlendMode = BlendMode.Add;
-            gameTitle.SetSize(512, 256);
+            gameTitle.SetSize(400, 200);
             gameTitle.SetPosition((Graphics.Width / 2) - (gameTitle.Width / 2), gameTitle.Height / 2);
 
             var settingsButton = new Button();
             settingsButton.Texture = cache.GetTexture2D("Textures/settingsButton.png"); // Set texture
             settingsButton.BlendMode = BlendMode.Add;
-            settingsButton.SetSize(50, 50);
+            settingsButton.SetSize(25, 25);
             settingsButton.SetPosition(Graphics.Width - settingsButton.Width - 20, 20);
             settingsButton.Name = "Settings";
             settingsButton.Opacity = 0.6f;
@@ -46,15 +64,15 @@ namespace TexasHoldemPoker
             var joinButton = new Button();
             joinButton.Texture = cache.GetTexture2D("Textures/joinGameButton.png"); // Set texture
             joinButton.BlendMode = BlendMode.Add;
-            joinButton.SetSize(256, 128);
-            joinButton.SetPosition(((Graphics.Width - joinButton.Width) / 5), Graphics.Height - 300);
+            joinButton.SetSize(150, 75);
+            joinButton.SetPosition(((Graphics.Width - joinButton.Width) / 5), Graphics.Height - 150);
             joinButton.Name = "JoinGame";
 
             var hostButton = new Button();
             hostButton.Texture = cache.GetTexture2D("Textures/hostGameButton.png"); // Set texture
             hostButton.BlendMode = BlendMode.Add;
-            hostButton.SetSize(256, 128);
-            hostButton.SetPosition(((Graphics.Width - hostButton.Width) / 5) * 4, Graphics.Height - 300);
+            hostButton.SetSize(150, 75);
+            hostButton.SetPosition(((Graphics.Width - hostButton.Width) / 5) * 4, Graphics.Height - 150);
             hostButton.Name = "HostGame";
 
             settingsButton.SetStyleAuto(null);
