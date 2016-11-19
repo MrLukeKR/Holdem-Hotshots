19d18f999a7149499ad473822d56ea271d7184fa
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-19 04:57:09 +0000

    Simple (temporary) example of how to use UrhoSharp

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Poker.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Poker.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using Urho;
+using Urho.Gui;
 
 namespace TexasHoldemPoker
 {
     class Poker : Urho.Application
     {
         public Poker(ApplicationOptions options = null) : base(options) { }
+        Camera camera;
+        Scene scene;
         protected override void Start()
         {
             base.Start();
-        }
+            var cache = ResourceCache;
+            var helloText = new Text()
+            {
+                Value = "Welcome to Texas Hold 'em Poker",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            helloText.SetColor(new Color(1f, 0f, 0f));
+            helloText.SetFont(font: cache.GetFont("Fonts/Anonymous Pro.ttf"), size: 30);
+            UI.Root.AddChild(helloText);
 
-        protected override void OnUpdate(float timeStep)
-        {
-            base.OnUpdate(timeStep);
+            Graphics.SetWindowIcon(cache.GetImage("Textures/UrhoIcon.png"));
+            Graphics.WindowTitle = "UrhoSharp Sample";
+
+            // Subscribe to Esc key:
+            Input.SubscribeToKeyDown(args => { if (args.Key == Key.Esc) Exit(); });
         }
     }
 }
