e30f95e917ec51727312ba4303192240e7aa0cf2
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-19 03:22:42 +0000

    Slowly starting to integrate UrhoSharp
    Former-commit-id: b45da3bea7dd8da0e7a39b313fb840c600b7efcc

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/GamePage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Urho.Forms;
+using Xamarin.Forms;
+
+namespace TexasHoldemPoker
+{
+    class GamePage : ContentPage
+    {
+
+        Slider selectedBarSlider, rotationSlider;
+        UrhoSurface gameSurface;
+
+        public GamePage()
+        {
+            gameSurface = new UrhoSurface();
+
+
+            rotationSlider = new Slider(0, 500, 250);
+
+            selectedBarSlider = new Slider(0, 5, 2.5);
+
+            Title = " UrhoSharp + Xamarin.Forms";
+            Content = new StackLayout
+            {
+                Padding = new Thickness(12, 12, 12, 40),
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = {
+        rotationSlider,
+        new Label { Text = "SELECTED VALUE:" },
+        selectedBarSlider,
+      }
+            };
+        }
+    }
+    }
+
