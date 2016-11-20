bfb54eaf7b553dbe04ee0b16a8f39230b27122ec
Jack Nicholson <psyjn1@nottingham.ac.uk>
2016-11-20 15:50:03 +0000

    Setup network engine folders, interface and factory

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/NetworkEngineInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TexasHoldemPoker.Game.NetworkEngine
+{
+    interface NetworkEngineInterface
+    {
+
+
+
+
+
+
+
+
+    }
+}
---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/NetworkEnginerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TexasHoldemPoker.Game.NetworkEngine
+{
+    class NetworkEnginerFactory
+    {
+
+
+#if __ANDROID__
+        public static NetworkEngineInterface create()
+        {
+
+            //Create Android network engine
+
+        }
+
+#endif
+
+#if __IOS__
+
+        public static NetworkEngineInterface create()
+        {
+
+            //Create IOS network engine
+
+
+
+
+        }
+
+
+#endif
+    }
+}
