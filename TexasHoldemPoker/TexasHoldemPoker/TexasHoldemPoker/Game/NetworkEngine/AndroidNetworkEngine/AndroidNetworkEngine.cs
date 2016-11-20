23ecbe4b8d9e607e4cec5570b76baa76560c5f99
Jack Nicholson <psyjn1@nottingham.ac.uk>
2016-11-20 18:05:36 +0000

    Added ListenForConnections() and PlayerConnection class

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/AndroidNetworkEngine/AndroidNetworkEngine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using MixedRealityPoker.Game.PokerObjects;
+
+namespace TexasHoldemPoker.Game.NetworkEngine.AndroidNetworkEngine
+{
+    class AndroidNetworkEngine : NetworkEngineInterface
+    {
+
+        private Socket serverListener;
+        private Lobby gameLobby;
+        private int listenerPortNumber = 8741;
+
+       public AndroidNetworkEngine()
+        {
+
+
+
+
+        }
+
+
+        private void listenForConnections()
+        {
+            
+            serverListener = new Socket(AddressFamily.InterNetwork ,SocketType.Stream,ProtocolType.Tcp);
+            serverListener.Bind(new IPEndPoint(0, listenerPortNumber));
+
+            while (true)
+            {
+
+                serverListener.Listen(0);
+
+                Socket connection = serverListener.Accept();
+
+                PlayerConnection player = new PlayerConnection(connection);
+
+                //TODO : Call function to add PlayerConnection to lobby
+
+            }
+
+
+        }
+
+        private void broadcastGameInfo()
+        {
+
+
+
+
+
+        }
+
+
+
+    }
+}
