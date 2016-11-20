f6e1a94c9dc65b2a491c7e80b2c47fbd82225912
Jack Nicholson <psyjn1@nottingham.ac.uk>
2016-11-20 18:05:36 +0000

    Added ListenForConnections() and PlayerConnection class

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/PlayerConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TexasHoldemPoker.Game.NetworkEngine
+{
+    /*
+     * This class handles all outgoing communication from the
+     * server to the player devices. The functions in this class
+     * will handle this communication
+     * 
+     */
+
+
+    class PlayerConnection
+    {
+
+        Socket connection;
+
+        public PlayerConnection(Socket connection)
+        {
+            this.connection = connection;
+
+
+
+        }
+
+
+
+
+    }
+}
