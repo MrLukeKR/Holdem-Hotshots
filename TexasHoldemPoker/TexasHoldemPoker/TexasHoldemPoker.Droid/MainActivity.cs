fe372ecf69c798b17c6c66ffc9394b978e19ecf5
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-20 21:35:12 +0000

    Add a splashscreen

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/MainActivity.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/MainActivity.cs
@@ -9,11 +9,15 @@ using Android.OS;
 
 namespace TexasHoldemPoker.Droid
 {
-	[Activity (Label = "Mixed Reality Poker", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+	[Activity (Label = "Mixed Reality Poker", Theme = "@style/splashscreen", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            base.SetTheme(global::Android.Resource.Style.ThemeHoloLight);
+
+
 			base.OnCreate (bundle);
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
