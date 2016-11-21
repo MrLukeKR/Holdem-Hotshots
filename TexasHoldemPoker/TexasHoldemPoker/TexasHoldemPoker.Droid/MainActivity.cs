a7cb5467daf6ba81b69e953ef3d54b0e373bb579
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-21 00:41:28 +0000

    Add buttonPress event handlers to menu with dummy implementations

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/MainActivity.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/MainActivity.cs
@@ -15,8 +15,7 @@ namespace TexasHoldemPoker.Droid
 		protected override void OnCreate (Bundle bundle)
 		{
             //base.Window.RequestFeature(WindowFeatures.ActionBar); //Not sure why this is needed... but it might be in the future
-            base.SetTheme(global::Android.Resource.Style.ThemeHoloLight);
-
+            base.SetTheme(global::Android.Resource.Style.ThemeHolo);
             base.OnCreate (bundle);
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
