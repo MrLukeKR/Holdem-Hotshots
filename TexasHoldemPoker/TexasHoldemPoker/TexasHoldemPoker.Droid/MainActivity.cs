d43b69936cd70d5fba184a1b85d40b8c568b8e8a
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-24 23:25:24 +0000

    Created join/host animations, started on "go back" handling

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/MainActivity.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/MainActivity.cs
@@ -2,21 +2,61 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
+using Urho.Droid;
 
 namespace TexasHoldemPoker.Droid
 {
 	[Activity (Label = "Mixed Reality Poker", Theme ="@style/splashscreen", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	public class MainActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
             base.Window.RequestFeature(WindowFeatures.ActionBar);
-            base.SetTheme(global::Android.Resource.Style.ThemeHolo);
+            base.SetTheme(global::Android.Resource.Style.ThemeNoTitleBarFullScreen);
             base.OnCreate (bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication(new App());
-            this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+            var mLayout = new AbsoluteLayout(this);
+            var surface = UrhoSurface.CreateSurface<Poker>(this);
+            mLayout.AddView(surface);
+            SetContentView(mLayout);
+        }
+
+        protected override void OnResume()
+        {
+            UrhoSurface.OnResume();
+            base.OnResume();
+        }
+
+        protected override void OnPause()
+        {
+            UrhoSurface.OnPause();
+            base.OnPause();
+        }
+
+        public override void OnLowMemory()
+        {
+            UrhoSurface.OnLowMemory();
+            base.OnLowMemory();
+        }
+
+        protected override void OnDestroy()
+        {
+            UrhoSurface.OnDestroy();
+            base.OnDestroy();
+        }
+
+        public override bool DispatchKeyEvent(KeyEvent e)
+        {
+            if (!UrhoSurface.DispatchKeyEvent(e))
+                return false;
+            return base.DispatchKeyEvent(e);
+        }
+
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            UrhoSurface.OnWindowFocusChanged(hasFocus);
+            base.OnWindowFocusChanged(hasFocus);
         }
     }
 }
