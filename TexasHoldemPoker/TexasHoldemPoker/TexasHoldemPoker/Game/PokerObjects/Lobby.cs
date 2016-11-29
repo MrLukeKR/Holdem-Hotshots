e1c58d74c2a04b5ebd4826bdc0e28315bcf1cab6
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-29 14:20:43 +0000

    Started work on the game loop and Lobby/Room objects

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerObjects/Lobby.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerObjects/Lobby.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldemPoker.Game.PokerObjects;
 
 namespace MixedRealityPoker.Game.PokerObjects
 {
-    class Lobby
+    class Lobby : PlayerCollection
     {
         private int numberOfPlayers;
         private Player[] players;
 
+        internal Player[] getPlayers()
+        {
+            return players;
+        }
+
+        
         public Lobby(int numberOfPlayers)
         {
             this.numberOfPlayers = numberOfPlayers;
             players = new Player[numberOfPlayers];
+        }
+        
+        public void addPlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removePlayer(Player player)
+        {
+            throw new NotImplementedException();
         }
     }
 }
