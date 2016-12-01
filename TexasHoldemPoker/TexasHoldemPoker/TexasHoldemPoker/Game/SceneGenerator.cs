284ce7ef69eac7b49a68fb6f8fde90e4ac5f51b2
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-12-01 14:03:13 +0000

    Implemented an MVC design pattern - Needs to be refactored at some point!

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/SceneGenerator.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/SceneGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Urho;
 using Urho.Audio;
+using Urho.Gui;
 using Urho.Resources;
 
 namespace TexasHoldemPoker.Game
@@ -25,6 +26,38 @@ namespace TexasHoldemPoker.Game
             return menuScene;
         }
 
+        public static void LoadJoiningScene(Node CameraNode, UI ui)
+        {
+            for (uint i = 0; i < 4; i++)
+                ui.Root.GetChild(i).Visible = false;
+
+
+            ui.Root.GetChild(5).Visible = false;
+            ui.Root.GetChild(5).Enabled = false;
+            ui.Root.GetChild(7).Visible = true;
+            ui.Root.GetChild(8).Visible = true;
+            ui.Root.GetChild(8).Enabled = true;
+            ui.Root.GetChild(9).Visible = false;
+            ui.Root.GetChild(9).Enabled = false;
+
+            ui.Root.GetChild("JoinLobby", true).Visible = true;
+            ui.Root.GetChild("JoinLobby", true).Enabled = true;
+
+            //Issues with movement on some devices
+            //Jump to position if animation causes issues:
+            /* CameraNode.Position = new Vector3(0.00544398f, 0.176587f, 0.159439f);
+               CameraNode.Rotation = new Quaternion(60f, -180f, 0f);
+            */
+
+            //DO LOADING
+
+            CameraHandler.panToJoin(CameraNode);
+            //Load hosting UI
+
+
+            // return playingScene;
+        }
+
         public static Scene CreateTableScene(ResourceCache cache)
         {
             Scene tableScene = new Scene();
@@ -39,6 +72,37 @@ namespace TexasHoldemPoker.Game
             musicSource.Play(music);
 
             return tableScene;
+        }
+
+        public static void LoadHostingScene(Node CameraNode, UI ui)
+        {
+            for (uint i = 0; i < 4; i++)
+                ui.Root.GetChild(i).Visible = false;
+
+
+            ui.Root.GetChild(5).Visible = false;
+            ui.Root.GetChild(5).Enabled = false;
+            ui.Root.GetChild(6).Visible = true;
+            ui.Root.GetChild(8).Visible = true;
+            ui.Root.GetChild(8).Enabled = true;
+            ui.Root.GetChild(9).Visible = false;
+            ui.Root.GetChild(9).Enabled = false;
+
+
+            ui.Root.GetChild("CreateLobby", true).Visible = true;
+            ui.Root.GetChild("CreateLobby", true).Enabled = true;
+
+            //Issues with movement on some devices
+            //Jump to position if animation causes issues:
+            /* CameraNode.Position = new Vector3(0.00544398f, 0.176587f, 0.159439f);
+               CameraNode.Rotation = new Quaternion(60f, -180f, 0f);
+            */
+
+            CameraNode.RemoveAllActions();
+            CameraHandler.panToHost(CameraNode);
+            //Load hosting UI
+
+            // return playingScene;
         }
     }
 }
