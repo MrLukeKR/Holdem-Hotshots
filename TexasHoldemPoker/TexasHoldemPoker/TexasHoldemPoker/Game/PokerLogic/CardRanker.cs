6fbcf88bfe8e8b7b2e0c6203e7ea2206a427e689
psymu <psymu@exmail.nottingham.ac.uk>
2016-12-14 23:43:54 +0000

    Update CardRanker - added suits and assigned values.

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerLogic/CardRanker.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerLogic/CardRanker.cs
@@ -1,20 +1,34 @@
 ﻿using PokerLogic;
+﻿import java.util.ArrayList
 
 namespace TexasHoldemPoker.Game.PokerObjects
 {
     static class CardRanker
     {
+        public static enum Hand{
+		ROYAL_FLUSH,
+		STRAIGHT_FLUSH,
+		FOUR_OF_A_KIND,
+		FULL_HOUSE,
+		FLUSH,
+		STRAIGHT,
+		THREE_OF_A_KIND,
+		TWO_PAIRS,
+		PAIR,
+		HIGH_CARD
+	};							//TODO: Consider using this enum instead of the list of constants
+	
+	public static final int ROYAL_FLUSH 	 = 10;
+	public static final int STRAIGHT_FLUSH 	 = 9;
+	public static final int FOUR_OF_A_KIND 	 = 8;
+	public static final int FULL_HOUSE       = 7;
+	public static final int FLUSH            = 6;
+	public static final int STRAIGHT         = 5;
+	public static final int THREE_OF_A_KIND  = 4;
+	public static final int TWO_PAIR         = 3;
+	public static final int PAIR             = 2;
+	public static final int HIGH_CARD        = 1;
+	
 
-        public static Player GetWinner(Player[] players)
-        {
-            // Go through all the players cards, call RankCards(players[1...2...3...n].GetCards()], Table.GetCards()) {Or something like that} and determine/return the winner
-
-            return null; // Replace this with the winning player
-        }
-
-        public static void RankCards(Card[] cards)  //SHOULDN'T BE OF TYPE VOID, BUT CHANGE THIS TO WHATEVER YOU DECIDE TO BE APPROPRIATE FOR RANKING (Perhaps an enum type?)
-        {
-
-        }
     }
 }
