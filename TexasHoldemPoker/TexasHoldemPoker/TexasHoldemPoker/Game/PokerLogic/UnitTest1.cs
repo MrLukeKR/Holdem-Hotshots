1b4ecb98a7d04148e15245aaad0f80869a3a6cf1
Yipin Jin <psyyj1@nottingham.ac.uk>
2016-12-15 16:51:14 +0000

    try to add Unittest1
    add a unitTest to test the method takeChips()

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerLogic/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void methodTest()
+        {
+            Player player = new Player("Billy", new Socket(SocketType.Stream, ProtocolType.Tcp));
+            player.chips = 30;
+            var.result1 = takechips(20);
+            var.result2 = takechips(40);
+            var expect1 = 10;
+            var expect2 = 0;
+            Assert.True(result == expect1 && result2 == expect2);
+        }
+    }
+}
