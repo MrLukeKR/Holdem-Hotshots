9148987596f02904dba224b5a9976267ee4da1d1
grt <psygrt@exmail.nottingham.ac.uk>
2016-12-30 16:27:11 +0000

    Style-Guide - whitespace edit of Game.cs

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerLogic/Game.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/PokerLogic/Game.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using Urho;
 
-namespace TexasHoldemPoker
-{
-    class PokerGame
-    {
-        private Table pokerTable;
-
-        public PokerGame(Room room, Scene tableScene, uint buyIn)
-        {
-            pokerTable = new Table(room, tableScene, buyIn);
-        }
-
-        public void run()
-        {
-            pokerTable.flop();
-            pokerTable.placeBets();
-
-            for (int i = 0; i < 2; i++) {
-                pokerTable.dealCards();
-                pokerTable.placeBets();
-            }
-
-            pokerTable.showdown();
-        }
+namespace TexasHoldemPoker{
+  class PokerGame{
+    private Table pokerTable;
+    public PokerGame(Room room, Scene tableScene, uint buyIn){
+      pokerTable = new Table(room, tableScene, buyIn);
     }
+    public void run(){
+      pokerTable.flop();
+      pokerTable.placeBets();
+      for (int i = 0; i < 2; i++){
+        pokerTable.dealCards();
+        pokerTable.placeBets();
+      }
+      pokerTable.showdown();
+    }
+  }
 }
