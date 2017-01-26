199b203525f539ccd8ece4897df28aa41e84070c
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2017-01-26 16:53:03 +0000

    Disabled PNG optimisation on iOS

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/Resources/Resource.Designer.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker.Droid/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace TexasHoldemPoker
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::TexasHoldemPoker.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
