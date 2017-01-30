82ac2ecf542be90349c74732d3f1cd316ef5f63d
Jack Nicholson <psyjn1@nottingham.ac.uk>
2017-01-30 14:43:28 +0000

    Multithreading implemented in server network engine

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/ServerNetworkEngine/broadcastThread.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TexasHoldemPoker.Game.NetworkEngine.ServerNetworkEngine
+{
+    class broadcastThread : Thread
+    {
+
+        private Socket broadcaster;
+        private int broadcastPortNumber = 8742;
+        private Room gameLobby;
+
+        public broadcastThread(Room gameLobby)
+        {
+            this.gameLobby = gameLobby;
+        }
+
+        public void Start()
+        {
+            broadcastGameInfo();
+        }
+
+        private void broadcastGameInfo()
+        {
+            this.broadcaster = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            this.broadcaster.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            IPEndPoint broadcastEndpoint = new IPEndPoint(IPAddress.Broadcast, broadcastPortNumber);
+            while (true)
+            {
+                sendBroadcast((listenerEndpoint.ToString() + gameLobby.getRoomSize().ToString()));
+                //TODO add delay between broadcasts
+            }
+        }
+
+        private void sendBroadcast(string message)
+        {
+            byte[] messageBuffer = Encoding.ASCII.GetBytes(message);
+            broadcaster.Send(messageBuffer);
+        }
+
+
+    }
+}
---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/ServerNetworkEngine/listenerThread.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TexasHoldemPoker.Game.NetworkEngine.ServerNetworkEngine
+{
+    class listenerThread : Thread
+    {
+
+        private Socket serverListener;
+        private int listenerPortNumber = 8741;
+        private IPEndPoint listenerEndpoint;
+        private Room gameLobby;
+
+        public listenerThread(Room gameLobby)
+        {
+            this.gameLobby = gameLobby;
+        }
+
+        public void Start()
+        {
+            listenForConnections();
+        }
+
+        private void listenForConnections()
+        {
+            serverListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenerEndpoint = new IPEndPoint(0, listenerPortNumber);
+            serverListener.Bind(listenerEndpoint);
+            while (true)
+            {
+                serverListener.Listen(0);
+                Socket connection = serverListener.Accept();
+                ClientConnection client = new ClientConnection(connection);
+
+                if (gameLobby.getRoomSize() >= gameLobby.MaxRoomSize)
+                {
+
+                    client.sendTooManyPlayers();
+                }
+                else
+                {
+                    string name = client.askName();
+                    gameLobby.addPlayer(new Player(name, 0, client));
+                }
+            }
+        }
+    }
+}
