38c1f1b4cf60c15fe498cfdbe9ddcf9c1afe0633
Jack Nicholson <psyjn1@nottingham.ac.uk>
2017-01-31 15:20:09 +0000

    Refactored the ServerNetworkEngine class to be a singleton

--- TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/ServerNetworkEngine/ServerNetworkEngine.cs
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/ServerNetworkEngine/ServerNetworkEngine.cs
@@ -6,7 +6,19 @@ using TexasHoldemPoker.Game.NetworkEngine.ServerNetworkEngine;
 
 namespace TexasHoldemPoker.Game.NetworkEngine.AndroidNetworkEngine{
   class ServerNetworkEngine : NetworkEngineInterface{
-   
+
+    private static ServerNetworkEngine networkEngine;
+
+    private ServerNetworkEngine()
+        {
+            //Leave blank for singleton design pattern
+        }
+    
+    public static ServerNetworkEngine getinstance()
+        {
+            return networkEngine;
+        }
+
     public void init(){
 
           gameLobby = new Room();
