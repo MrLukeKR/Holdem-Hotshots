c7980009abbeb620d2044d0c72c4724a3cfacd98
Jack Nicholson <psyjn1@nottingham.ac.uk>
2017-02-01 12:39:07 +0000

    Create interface for clientConnection

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/Game/NetworkEngine/ServerNetworkEngine/ClientInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TexasHoldemPoker.Game.NetworkEngine.ServerNetworkEngine
+{
+    interface ClientInterface
+    {
+        public String getName();
+        public void sendTooManyPlayers();
+        public void sendPlayerKicked();
+        public void getPlayerAction();
+        public int getBet();
+
+
+    }
+}
