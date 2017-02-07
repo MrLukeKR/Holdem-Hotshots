03162e11f51adbc7a175e0a4082f474107af9a3b
Jack Nicholson <psyjn1@nottingham.ac.uk>
2017-02-07 16:30:52 +0000

    CommandListenerThread added
    Former-commit-id: 4789a4f2f948609ad0a1b50523f45a71180b2d67

---
+++ HoldemHotshots/HoldemHotshots/Networking/ClientNetworkEngine/CommandListenerThread.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HoldemHotshots.Networking.ClientNetworkEngine
+{
+    class CommandListenerThread
+    {
+        private CommandManager commandmanager;
+
+        public CommandListenerThread(ServerConnection connection)
+        {
+            this.commandmanager = commandmanager.getInstance(connection);
+        }
+
+        public void Start()
+        {
+            listenForCommands();
+        }
+
+        private void listenForCommands()
+        {
+            while (true)
+            {
+                String command = connection.getResponse();
+                commandmanager.runCommand(command);
+            }
+        }
+
+    }
+}
