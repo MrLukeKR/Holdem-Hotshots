132ac50672feb949e6d117cb2104aef2bb9d624e
Jack Nicholson <psyjn1@nottingham.ac.uk>
2017-02-09 12:44:29 +0000

    Monitor threads intergrated into the correct connection classes
    Former-commit-id: 629c4c55d198b17575cafc3b6a7a44ec4b33e2d3

--- HoldemHotshots/HoldemHotshots/Networking/ClientNetworkEngine/ServerConnection.cs
+++ HoldemHotshots/HoldemHotshots/Networking/ClientNetworkEngine/ServerConnection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Net.Sockets;
+using HoldemHotshots.Networking;
 
 namespace HoldemHotshots
 {
@@ -8,10 +9,13 @@ namespace HoldemHotshots
     {
 
         private Socket connection;
+        private ServerConnectionMonitorThread monitorThread;
 
         public ServerConnection(Socket connection)
         {
             this.connection = connection;
+            this.monitorThread = new ServerConnectionMonitorThread(connection);
+            monitorThread.start();
         }
 
         public void sendMessage(String command)
