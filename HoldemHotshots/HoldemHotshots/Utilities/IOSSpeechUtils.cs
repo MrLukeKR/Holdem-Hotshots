3eff503c5786c7858d7156115bea33af0cb6ba77
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2017-03-16 11:13:18 +0000

    Implemented Text-To-Speech
    Former-commit-id: b5a1bc0374286e19bbfd51965186c68cfbaaa38f

---
+++ HoldemHotshots/HoldemHotshots/Utilities/IOSSpeechUtils.cs
@@ -0,0 +1,16 @@
+﻿using AVFoundation;
+
+namespace HoldemHotshots.Utilities
+{
+    class IOSSpeechUtils
+    {
+        AVSpeechSynthesizer speechSynthesizer = new AVSpeechSynthesizer();
+
+        public void Speak(string message)
+        {
+            var speechUtterance = new AVSpeechUtterance(message);
+
+            speechSynthesizer.SpeakUtterance(speechUtterance);
+        }
+    }
+}
