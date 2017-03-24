c64131e9e998288b7a91d7881fca435ad6369b0c
psyxl10 <psyxl10@exmail.nottingham.ac.uk>
2017-03-24 23:13:07 +0000

    Added test for Card.cs
    Former-commit-id: 390ef7bc16f7bd0267ba945bf4e0a55b95793561

---
+++ Testing/LogicUnitTesting/LogicUnitTesting/Card.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicUnitTesting
+{
+    [TestClass]
+    public class Card
+    {
+        [TestMethod]
+        public void inti()
+        {
+            // how to test?
+        }
+    }
+}
