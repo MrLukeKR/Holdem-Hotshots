477607beb197fd592ef5822efd3dcab50f5c7652
Jack Nicholson <psyjn1@nottingham.ac.uk>
2017-04-04 18:37:20 +0100

    Created and implemented Encryptor Class
    Former-commit-id: 74934bf031f862940d8179d2653db59e5a076824

---
+++ HoldemHotshots/HoldemHotshots/Utilities/Encryptor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace HoldemHotshots.Utilities
+{
+    class Encryptor
+    {
+        
+        private RijndaelManaged cryptoManager;
+        private ICryptoTransform encyptorCipher;
+        private ICryptoTransform decyptorCipher;
+
+        public Encryptor()
+        {
+            cryptoManager = new RijndaelManaged();
+            cryptoManager.GenerateKey();
+            cryptoManager.GenerateIV();
+
+            encyptorCipher = cryptoManager.CreateEncryptor(cryptoManager.Key, cryptoManager.IV);
+            decyptorCipher = cryptoManager.CreateDecryptor(cryptoManager.Key, cryptoManager.IV);
+           
+        }
+
+        public Encryptor(byte[] key,byte[] iv)
+        {
+            cryptoManager = new RijndaelManaged();
+            cryptoManager.Key = key;
+            cryptoManager.IV = iv;
+
+            encyptorCipher = cryptoManager.CreateEncryptor(cryptoManager.Key, cryptoManager.IV);
+            decyptorCipher = cryptoManager.CreateDecryptor(cryptoManager.Key, cryptoManager.IV);
+
+        }
+
+        public string EncyptString(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentNullException("text");
+
+            MemoryStream memStream = new MemoryStream();
+            CryptoStream encryptSteam = new CryptoStream(memStream,encyptorCipher,CryptoStreamMode.Write);
+            StreamWriter streamWriter = new StreamWriter(encryptSteam);
+
+            streamWriter.Write(text);
+
+            return Convert.ToBase64String(memStream.ToArray());
+        }
+
+
+        public String DecryptString(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentNullException("text");
+
+
+            var cipher = Convert.FromBase64String(text);
+
+            MemoryStream memStream = new MemoryStream(cipher);
+            CryptoStream decryptSteam = new CryptoStream(memStream, decyptorCipher, CryptoStreamMode.Read);
+            StreamReader streamReader = new StreamReader(decryptSteam);
+
+            string decryptedText = streamReader.ReadToEnd();
+
+            return decryptedText;
+        }
+
+        
+
+
+    }
+}
