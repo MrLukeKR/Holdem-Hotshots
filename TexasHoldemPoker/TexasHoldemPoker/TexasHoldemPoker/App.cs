48a80cccb806b57c122f234ea05fd22fe53896fb
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2018-03-14 16:54:02 +0000

    Removed gitignore
    Former-commit-id: 4fd01dd6d66792a15ef8cf9a7f213d77eb6b64cf

---
+++ TexasHoldemPoker/TexasHoldemPoker/TexasHoldemPoker/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TexasHoldemPoker
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Texas Hold 'em Poker"
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
